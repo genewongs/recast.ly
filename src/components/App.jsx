17b85829df5df35a265d3108bc92ef8993051438
Gene Wong <gene.h.wong@gmail.com>
2022-03-07 17:32:04 -0800

    passing test for VideoPlayer and currently working on App portion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,43 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false
+    };
+  }
+
+  onClickHandler() {
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={exampleVideoData[0]} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={exampleVideoData} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+/*
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,6 +47,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
+        <VideoPlayer video={exampleVideoData[0]} />
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
@@ -20,6 +58,7 @@ var App = () => (
   </div>
 );
 
+*/
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
